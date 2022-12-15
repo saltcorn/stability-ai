5436a6299daa93946218a85427cb93b767d23d1a
Tom Nielsen <tanielsen@gmail.com>
2022-12-15 23:04:25 +0000

    api key

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
+const Workflow = require("@saltcorn/data/models/workflow");
+const Form = require("@saltcorn/data/models/form");
+
 const { generateAsync } = require("stability-client");
 
-const stability_generate = {
-  run: async (prompt) => {
-    try {
-      const { res, images } = await generateAsync({
-        prompt,
-        apiKey: process.env.DREAMSTUDIO_API_KEY,
-      });
-      console.log(images);
-    } catch (e) {
-      // ...
-    }
+const configuration_workflow = () =>
+  new Workflow({
+    steps: [
+      {
+        name: "API key",
+        form: async (context) => {
+          return new Form({
+            fields: [
+              {
+                name: "api_key",
+                label: "API key",
+                sublabel: "From Dreamstudio account settings",
+                type: "String",
+              },
+            ],
+          });
+        },
+      },
+    ],
+  });
+
+const functions = ({ api_key }) => ({
+  stability_generate: {
+    run: async (prompt) => {
+      try {
+        const { res, images } = await generateAsync({
+          prompt,
+          apiKey: api_key,
+        });
+        console.log(images);
+      } catch (e) {
+        // ...
+      }
+    },
+    isAsync: true,
+    description: "Generate an image",
+    arguments: [{ name: "prompt", type: "String" }],
   },
-  isAsync: true,
-  description: "Generate an image",
-  arguments: [{ name: "prompt", type: "String" }],
-};
+});
 module.exports = {
   sc_plugin_api_version: 1,
-  functions: { stability_generate },
+  configuration_workflow,
+  functions,
 };
